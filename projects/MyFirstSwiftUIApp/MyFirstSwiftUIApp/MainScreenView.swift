230814a20cf1121c22d95872ea74518e936ed914
Emil Atanasov <heitara@gmail.com>
2023-10-12 18:15:07 +0300

    feat: първи iOS проекти

---
+++ projects/MyFirstSwiftUIApp/MyFirstSwiftUIApp/MainScreenView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  MyFirstSwiftUIApp
+//
+//  Created by Emil Atanasov on 10/09/23.
+//
+
+import SwiftUI
+
+struct MainScreenView: View {
+    var body: some View {
+        VStack {
+            let _ = print("demo")
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Image(systemName: "square.and.arrow.up.trianglebadge.exclamationmark")
+                .foregroundColor(Color.orange)
+                
+            Text("Hello, swift!")
+                .foregroundColor(.red)
+                .background(Color.blue)
+            DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
+            Button {
+                print("hello!")
+            } label: {
+                Text("Hi")
+            }
+            .buttonStyle(.bordered)
+            .tint(.green)
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainScreenView()
+//    Text("Hello")
+}
---
+++ projects/MyFirstSwiftUIApp/MyFirstSwiftUIApp/MyFirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUIAppApp.swift
+//  MyFirstSwiftUIApp
+//
+//  Created by Emil Atanasov on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainScreenView()
+        }
+    }
+}
